eba53e83795a378300362c8181b10dacb49776f1
Satyam Raghuvanshi <satyamraghuvanshi220ct@gmail.com>
2025-12-24 18:01:52 +0530

    Remove unnecessary files (deploy scripts, tests, setup.py)

--- README.md
+++ README.md
@@ -123,5 +123,4 @@ This project is licensed under the MIT License - see the [LICENSE](LICENSE) file
 
 ---
 
-## Developed with ❤️ by Satyam Raghuvanshi 
-
+## Developed with ❤️ by Satyam Raghuvanshi
--- tests/test_urls.py
+++
@@ -1,8 +0,0 @@
-from phishguard.extractors.url_extractor import URLExtractor
-
-def test_url_extractor():
-    url = "https://www.google.com"
-    extractor = URLExtractor(url)
-    features = extractor.extract_features()
-    assert features['has_https'] == 1
-    assert features['is_ip'] == 0
